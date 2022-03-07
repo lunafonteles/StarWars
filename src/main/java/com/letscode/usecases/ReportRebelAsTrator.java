02c0e5856463f28475b1a62e64f6632a1a66ef77
lauraemme <lauraemmendoerfer@gmail.com>
2022-03-07 15:12:13 -0300

    add reportAsTraitor

--- src/main/java/com/letscode/usecases/ReportRebelAsTrator.java
+++
@@ -1,8 +0,0 @@
-package com.letscode.usecases;
-
-public class ReportRebelAsTrator {
-
-    //se 3 rebeldes reportarem uma pessoa, ela se torna traidora
-    //não pode negociar recursos nem manipular inventário, nem ser exibido em relatórios.
-    //
-}
