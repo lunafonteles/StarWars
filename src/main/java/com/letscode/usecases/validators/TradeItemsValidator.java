0cadc89068a88cbdf46fd14df3a102106988d46f
lauraemme <lauraemmendoerfer@gmail.com>
2022-03-12 14:00:31 -0300

    created TradeItems

--- src/main/java/com/letscode/usecases/validators/TradeItemsValidator.java
+++ src/main/java/com/letscode/usecases/validators/TradeItemsValidator.java
@@ -1,5 +1,7 @@
 package com.letscode.usecases.validators;
 
+import com.letscode.domains.Inventory;
+import com.letscode.domains.ItemsToTrade;
 import com.letscode.domains.Rebel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -12,5 +14,21 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TradeItemsValidator {
 
+    public boolean oneOfThemIsTraitor(Rebel rebel1, Rebel rebel2){
+        return rebel1.isTraitor() || rebel2.isTraitor();
+    }
 
+    public boolean hasTheSamePoints(ItemsToTrade givenItems, ItemsToTrade receivedItemns){
+        return givenItems.getPoints() == receivedItemns.getPoints();
+    }
+
+    public boolean rebelHasTheItemsToTrade(Rebel rebel, ItemsToTrade itemsToTrade){
+        Inventory rebelInventory = rebel.getInventory();
+        boolean hasTheAmountOfFood = rebelInventory.getFoodAmount() >= itemsToTrade.getFoodAmount();
+        boolean hasTheAmountOfGuns = rebelInventory.getGunsAmount() >= itemsToTrade.getGunsAmount();
+        boolean hasTheAmountOfWater = rebelInventory.getWaterAmount() >= itemsToTrade.getWaterAmount();
+        boolean hasTheAmountOfMunition = rebelInventory.getMunitionAmount() >= itemsToTrade.getMunitionAmount();
+
+        return (hasTheAmountOfFood && hasTheAmountOfGuns && hasTheAmountOfMunition && hasTheAmountOfWater);
+    }
 }
