67cd6fff21d35adc65ad6765e07350d879b6482a
Mahhh <73047662+marisasales@users.noreply.github.com>
2022-03-13 23:20:48 -0300

    update TradeItems and TradeItemsValidator

--- src/main/java/com/letscode/usecases/TradeItems.java
+++ src/main/java/com/letscode/usecases/TradeItems.java
@@ -6,6 +6,7 @@ import com.letscode.exceptions.ValidationException;
 import com.letscode.gateways.RebelPersistenceGateway;
 import com.letscode.usecases.validators.TradeItemsValidator;
 import lombok.RequiredArgsConstructor;
+import lombok.val;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,30 +20,37 @@ public class TradeItems {
 
         Rebel rebel1 = rebelPersistenceGateway.getById(rebel1Id);
         Rebel rebel2 = rebelPersistenceGateway.getById(rebel2Id);
+        val validationErrors = tradeItemsValidator.validate(rebel1, rebel2, givenItems, receivedItems);
 
-
-        if (tradeItemsValidator.oneOfThemIsTraitor(rebel1, rebel2)) {
-            throw new ValidationException("Um deles é traidor");
-        }
-        if (!tradeItemsValidator.hasTheSamePoints(givenItems, receivedItems)) {
-            throw new ValidationException("A quantidade de pontos a trocar é diferente");
-        }
-        if (!tradeItemsValidator.rebelHasTheItemsToTrade(rebel1, givenItems)
-                || !tradeItemsValidator.rebelHasTheItemsToTrade(rebel2, receivedItems)) {
-            throw new ValidationException("Um dos rebeldes não tem itens para concluir a troca");
+        if (!validationErrors.isEmpty()) {
+            throw new ValidationException(validationErrors);
         }
 
-        updateInventory(rebel1, receivedItems, givenItems);
-        updateInventory(rebel2, givenItems, receivedItems);
+        rebelPersistenceGateway.updateInventory(rebel1, receivedItems, givenItems);
+        rebelPersistenceGateway.updateInventory(rebel2, givenItems, receivedItems);
 
-    }
-
-    private void updateInventory(Rebel rebel, Inventory givenItems, Inventory receivedItems) {
-        rebel.getInventory().setGunsAmount(rebel.getInventory().getGunsAmount() - givenItems.getGunsAmount() + receivedItems.getGunsAmount());
-        rebel.getInventory().setMunitionAmount(rebel.getInventory().getMunitionAmount() - givenItems.getMunitionAmount() + receivedItems.getMunitionAmount());
-        rebel.getInventory().setWaterAmount(rebel.getInventory().getWaterAmount() - givenItems.getWaterAmount() + receivedItems.getWaterAmount());
-        rebel.getInventory().setFoodAmount(rebel.getInventory().getFoodAmount() - givenItems.getFoodAmount() + receivedItems.getFoodAmount());
-
-        rebelPersistenceGateway.save(rebel);
+//        if (tradeItemsValidator.oneOfThemIsTraitor(rebel1, rebel2)) {
+//            throw new ValidationException("Um deles é traidor");
+//        }
+//        if (!tradeItemsValidator.hasTheSamePoints(givenItems, receivedItems)) {
+//            throw new ValidationException("A quantidade de pontos a trocar é diferente");
+//        }
+//        if (!tradeItemsValidator.rebelHasTheItemsToTrade(rebel1, givenItems)
+//            || !tradeItemsValidator.rebelHasTheItemsToTrade(rebel2, receivedItems)) {
+//            throw new ValidationException("Um dos rebeldes não tem itens para concluir a troca");
+//        }
+//
+//        updateInventory(rebel1, receivedItems, givenItems);
+//        updateInventory(rebel2, givenItems, receivedItems);
+//
+//    }
+//
+//    private void updateInventory(Rebel rebel, Inventory givenItems, Inventory receivedItems) {
+//        rebel.getInventory().setGunsAmount(rebel.getInventory().getGunsAmount() - givenItems.getGunsAmount() + receivedItems.getGunsAmount());
+//        rebel.getInventory().setMunitionAmount(rebel.getInventory().getMunitionAmount() - givenItems.getMunitionAmount() + receivedItems.getMunitionAmount());
+//        rebel.getInventory().setWaterAmount(rebel.getInventory().getWaterAmount() - givenItems.getWaterAmount() + receivedItems.getWaterAmount());
+//        rebel.getInventory().setFoodAmount(rebel.getInventory().getFoodAmount() - givenItems.getFoodAmount() + receivedItems.getFoodAmount());
+//
+//        rebelPersistenceGateway.save(rebel);
     }
 }
--- src/main/java/com/letscode/usecases/validators/TradeItemsValidator.java
+++ src/main/java/com/letscode/usecases/validators/TradeItemsValidator.java
@@ -5,24 +5,76 @@ import com.letscode.domains.Rebel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 @Component
 @RequiredArgsConstructor
 public class TradeItemsValidator {
-    public boolean oneOfThemIsTraitor(Rebel rebel1, Rebel rebel2){
-        return rebel1.isTraitor() || rebel2.isTraitor();
+//    public boolean oneOfThemIsTraitor(Rebel rebel1, Rebel rebel2){
+//        return rebel1.isTraitor() || rebel2.isTraitor();
+//    }
+//
+//    public boolean hasTheSamePoints(Inventory givenItems, Inventory receivedItemns){
+//        return givenItems.getPoints() == receivedItemns.getPoints();
+//    }
+//
+//    public boolean rebelHasTheItemsToTrade(Rebel rebel, Inventory itemsToTrade){
+//        Inventory rebelInventory = rebel.getInventory();
+//        boolean hasTheAmountOfFood = rebelInventory.getFoodAmount() >= itemsToTrade.getFoodAmount();
+//        boolean hasTheAmountOfGuns = rebelInventory.getGunsAmount() >= itemsToTrade.getGunsAmount();
+//        boolean hasTheAmountOfWater = rebelInventory.getWaterAmount() >= itemsToTrade.getWaterAmount();
+//        boolean hasTheAmountOfMunition = rebelInventory.getMunitionAmount() >= itemsToTrade.getMunitionAmount();
+//
+//        return (hasTheAmountOfFood && hasTheAmountOfGuns && hasTheAmountOfMunition && hasTheAmountOfWater);
+//    }
+
+    public List<String> validate(Rebel rebel1, Rebel rebel2, Inventory givenItems, Inventory receivedItems) {
+        List<String> validationErrors = new ArrayList<>();
+
+        if (Objects.equals(rebel1.getId(), rebel2.getId())) {
+            validationErrors.add("Você não pode negociar com você mesmo");
+        }
+
+        if (rebel1.isTraitor() || rebel2.isTraitor()) {
+            validationErrors.add("Traidores não podem negociar");
+        }
+
+        hasEnoughPointsToTrade(rebel1, givenItems, validationErrors);
+        hasEnoughPointsToTrade(rebel2, receivedItems, validationErrors);
+
+        hasItemsToTrade(rebel1, givenItems, validationErrors);
+        hasItemsToTrade(rebel2, receivedItems, validationErrors);
+
+        if (!Objects.equals(givenItems.getPoints(), receivedItems.getPoints())) {
+            validationErrors.add("Os pontos devem ser iguais");
+        }
+
+        return validationErrors;
     }
 
-    public boolean hasTheSamePoints(Inventory givenItems, Inventory receivedItemns){
-        return givenItems.getPoints() == receivedItemns.getPoints();
+    private void hasItemsToTrade(Rebel rebel, Inventory inventory, List<String> validationErrors) {
+        if (inventory.getGunsAmount() > rebel.getInventory().getGunsAmount()) {
+            validationErrors.add(rebel.getName() + " não tem armas suficientes para negociar");
+        }
+
+        if (inventory.getMunitionAmount() > rebel.getInventory().getMunitionAmount()) {
+            validationErrors.add(rebel.getName() + " não tem munições suficientes para negociar");
+        }
+
+        if (inventory.getWaterAmount() > rebel.getInventory().getWaterAmount()) {
+            validationErrors.add(rebel.getName() + " não tem água suficientes para negociar");
+        }
+
+        if (inventory.getFoodAmount() > rebel.getInventory().getFoodAmount()) {
+            validationErrors.add(rebel.getName() + " não tem comida suficientes para negociar");
+        }
     }
 
-    public boolean rebelHasTheItemsToTrade(Rebel rebel, Inventory itemsToTrade){
-        Inventory rebelInventory = rebel.getInventory();
-        boolean hasTheAmountOfFood = rebelInventory.getFoodAmount() >= itemsToTrade.getFoodAmount();
-        boolean hasTheAmountOfGuns = rebelInventory.getGunsAmount() >= itemsToTrade.getGunsAmount();
-        boolean hasTheAmountOfWater = rebelInventory.getWaterAmount() >= itemsToTrade.getWaterAmount();
-        boolean hasTheAmountOfMunition = rebelInventory.getMunitionAmount() >= itemsToTrade.getMunitionAmount();
-
-        return (hasTheAmountOfFood && hasTheAmountOfGuns && hasTheAmountOfMunition && hasTheAmountOfWater);
+    private void hasEnoughPointsToTrade(Rebel rebel, Inventory inventory, List<String> validationErrors) {
+        if (rebel.getInventory().getPoints() < inventory.getPoints()) {
+            validationErrors.add(rebel.getName() + " não tem pontos suficientes para negociar");
+        }
     }
 }
