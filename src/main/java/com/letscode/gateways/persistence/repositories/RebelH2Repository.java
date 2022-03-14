6db1f7d1e73a0cb8246fd47b4bae71254520f0b8
Mahhh <73047662+marisasales@users.noreply.github.com>
2022-03-14 18:32:18 -0300

    remove h2

--- src/main/java/com/letscode/gateways/persistence/repositories/RebelH2Repository.java
+++
@@ -1,7 +0,0 @@
-package com.letscode.gateways.persistence.repositories;
-import com.letscode.gateways.persistence.Entities.RebelEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RebelH2Repository extends JpaRepository<RebelEntity, Long> {
-
-}
